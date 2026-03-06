74a57ee7d94fa6c9523ffaedc96e1d45a4ba69fa
garvthakur123 <garvthakur123@gmail.com>
2026-03-06 20:51:36 +0100

    Description lenght control in job posting

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
+import ViewAllComponents from './components/ViewAllComponents'
 
 const App = () => {
 
@@ -14,13 +15,7 @@ const App = () => {
       <Hero title={title} subTitle={subtitle}/>
       <HomeCards />
       <JobListings />
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-          >View All Jobs</a
-        >
-      </section>
+      <ViewAllComponents />
     </div>
   )
 }
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,36 +1,54 @@
-import React from 'react'
+import { useState } from 'react';
 
 const JobListing = ({ job }) => {
+
+    const [showFullDesc, setShowFullDesc] = useState(false);
+
+    let description = job.description;
+    if (!showFullDesc) {
+        description = description.substring(0, 90) + '...';
+    }
+
+    const setShow = () => {
+        setShowFullDesc(prev => !prev)
+    }
     return (
-        <div>                            <div className='bg-white rounded-xl shadow-md relative'>
-            <div className="p-4">
-                <div className="mb-6">
-                    <div className="text-gray-600 my-2">{job.type}</div>
-                    <h3 className="text-xl font-bold">{job.title}</h3>
-                </div>
-
-                <div className="mb-5">
-                    {job.description}
-                </div>
-
-                <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
-
-                <div className="border border-gray-100 mb-5"></div>
-
-                <div className="flex flex-col lg:flex-row justify-between mb-4">
-                    <div className="text-orange-700 mb-3">
-                        <i className="fa-solid fa-location-dot text-lg"></i>
-                        {job.location}
+        <div>
+            <div className='bg-white rounded-xl shadow-md relative'>
+                <div className="p-4">
+                    <div className="mb-6">
+                        <div className="text-gray-600 my-2">{job.type}</div>
+                        <h3 className="text-xl font-bold">{job.title}</h3>
                     </div>
-                    <a
-                        href={`/job/${job.id}`}
-                        className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                    >
-                        Read More
-                    </a>
+
+                    <div className="mb-5">
+                        {description}
+                        <br />
+                        <br />
+                        {showFullDesc ? <button onClick={setShow} className='text-indigo-500 mb-5 hover:text-indigo-600'>Less</button> :
+                            <button onClick={setShow} className='text-indigo-500 mb-5 hover:text-indigo-600'>More</button>
+                        }
+                    </div>
+
+                    <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
+
+                    <div className="border border-gray-100 mb-5"></div>
+
+                    <div className="flex flex-col lg:flex-row justify-between mb-4">
+                        <div className="text-orange-700 mb-3">
+                            <i className="fa-solid fa-location-dot text-lg"></i>
+                            {job.location}
+                        </div>
+                        <a
+                            href={`/job/${job.id}`}
+                            className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                        >
+                            Read More
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div></div>
+        </div>
     )
 }
 
