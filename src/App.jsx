5804b3549dac5212e75692be8aafc23ca6706c29
garvthakur123 <garvthakur123@gmail.com>
2026-03-06 21:28:01 +0100

    Added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import HomePage from './pages/HomePage';
 import MainLayout from './pages/MainLayout';
 import JobsPage from './pages/JobsPage';
+import NotFound from './pages/NotFound';
 
 
 const router = createBrowserRouter(
@@ -10,6 +11,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element = {<JobsPage />}/>
+      <Route path='*' element={<NotFound />} />
     </Route>
   )
 );
